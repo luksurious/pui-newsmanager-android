e71c14769d327e29ccb8f3038465d81f8a3ed655
Lukas Brückner <me@luksurious.me>
2018-12-21 01:17:44 +0100

    add login activity

--- app/src/main/java/es/upm/hcid/newsmanager/MainActivity.java
+++ app/src/main/java/es/upm/hcid/newsmanager/MainActivity.java
@@ -1,7 +1,13 @@
 package es.upm.hcid.newsmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_login:
+                Intent openLogin = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(openLogin);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
